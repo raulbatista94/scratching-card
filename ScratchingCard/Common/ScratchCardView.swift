f6c0f87e936b2fc54ad331929d16f7fb6801a915
Raul Batista <raulbatista94@gmail.com>
2024-03-24 20:23:50 +0100

    [feat] Make the scratch view reusable

--- ScratchingCard/Common/ScratchCardView.swift
+++ ScratchingCard/Common/ScratchCardView.swift
@@ -8,11 +8,68 @@
 import SwiftUI
 
 struct ScratchCardView: View {
+    @Binding var scratchedPoints: [CGPoint]
+    private let couponCode: String
+    init(
+        couponCode: String,
+        scratchedPoints: Binding<[CGPoint]>
+    ) {
+        self.couponCode = couponCode
+        self._scratchedPoints = scratchedPoints
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            ZStack{
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(LinearGradient(
+                        colors: [
+                            Color(red: 22/255, green: 83/255, blue: 182/255),
+                            Color(red: 9/255, green: 41/255, blue: 93/255),
+                            Color(red: 4/255, green: 25/255, blue: 61/255)
+                        ],
+                        startPoint: .top,
+                        endPoint: .bottom)
+                    )
+
+                Text("Welcome to O2")
+                    .font(.title2)
+                    .foregroundStyle(Color.white)
+            }
+            .frame(width: 205, height: 205)
+
+            VStack {
+                Image(systemName: "apple.logo")
+                Text(couponCode)
+                    .minimumScaleFactor(0.7)
+                    .padding(.horizontal)
+            }
+            .frame(width: 200, height: 200)
+            .background(
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color.white)
+                    .stroke(.black, style: StrokeStyle(lineWidth: 5))
+            )
+            .padding()
+            .mask(
+                Path { path in
+                    path.addLines(scratchedPoints)
+                }
+                    .stroke(style: StrokeStyle(
+                        lineWidth: 20,
+                        lineCap: .round,
+                        lineJoin: .round)
+                    )
+            )
+        }
     }
 }
 
+#if DEBUG
 #Preview {
-    ScratchCardView()
+    ScratchCardView(
+        couponCode: UUID().uuidString,
+        scratchedPoints: .constant([])
+    )
 }
+#endif
--- ScratchingCard/MainScren/MainScreenView.swift
+++ ScratchingCard/MainScren/MainScreenView.swift
@@ -16,58 +16,19 @@ struct MainScreenView: View {
     }
 
     var body: some View {
-        ZStack {
-            ZStack{
-                RoundedRectangle(cornerRadius: 20)
-                    .fill(LinearGradient(
-                        colors: [
-                            Color(red: 22/255, green: 83/255, blue: 182/255),
-                            Color(red: 9/255, green: 41/255, blue: 93/255),
-                            Color(red: 4/255, green: 25/255, blue: 61/255)
-                        ],
-                        startPoint: .top,
-                        endPoint: .bottom)
-                    )
-
-                Text("Welcome to O2")
-                    .font(.title2)
-                    .foregroundStyle(Color.white)
-            }
-            .frame(width: 205, height: 205)
-
-            VStack {
-                Image(systemName: "apple.logo")
-                Text(viewModel.couponCode)
-                    .minimumScaleFactor(0.7)
-                    .padding(.horizontal)
-            }
-            .frame(width: 200, height: 200)
-            .background(
-                RoundedRectangle(cornerRadius: 20)
-                    .fill(Color.white)
-                    .stroke(.black, style: StrokeStyle(lineWidth: 5))
-            )
-            .padding()
-            .mask(
-                Path { path in
-                    path.addLines(viewModel.scratchedPoints)
+        ScratchCardView(
+            couponCode: viewModel.couponCode,
+            scratchedPoints: $viewModel.scratchedPoints
+        )
+        .gesture(
+            DragGesture(minimumDistance: 0, coordinateSpace: .local)
+                .onChanged {
+                    viewModel.send(action: .dragGestureLocationChanged($0.location))
                 }
-                .stroke(style: StrokeStyle(
-                    lineWidth: 20,
-                    lineCap: .round,
-                    lineJoin: .round)
-                )
-            )
-            .gesture(
-                DragGesture(minimumDistance: 0, coordinateSpace: .local)
-                    .onChanged {
-                        viewModel.send(action: .dragGestureLocationChanged($0.location))
-                    }
-                    .onEnded { _ in
-                        viewModel.send(action: .didFinishDragGesture)
-                    }
-            )
-        }
+                .onEnded { _ in
+                    viewModel.send(action: .didFinishDragGesture)
+                }
+        )
         .onAppear {
             viewModel.bind()
         }
